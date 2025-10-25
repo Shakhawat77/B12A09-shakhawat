f678436373e8aaac184c3702d0560ffcbae9c246
Shakhawat77 <shoadshakhawat7@gmail.com>
2025-10-25 12:10:47 +0600

    connect to netlify

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -6,9 +6,17 @@ const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
   const location = useLocation();
 
-//   if (loading) {
-//     return <p className="text-center font-semibold">Loading...</p>;
-//   }
+  if (loading) {
+    return (
+      <p className="text-center font-semibold text-2xl my-20">
+        <span className="loading loading-ring loading-xs"></span>
+        <span className="loading loading-ring loading-sm"></span>
+        <span className="loading loading-ring loading-md"></span>
+        <span className="loading loading-ring loading-lg"></span>
+        <span className="loading loading-ring loading-xl"></span>
+      </p>
+    );
+  }
 
   if (!user) {
     return <Navigate to="/signin" state={{ from: location }} replace />;
